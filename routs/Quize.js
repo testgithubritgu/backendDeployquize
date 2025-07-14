ad098c72a09a5ae14b619590747c1d50619075f2
testgithubritgu <swapnilraut1698@gmail.com>
2025-07-14 12:05:06 +0530

    added backend files

--- routs/Quize.js
+++ routs/Quize.js
@@ -8,15 +8,15 @@ router.get("/:id", (req, res) => {
     const id = req.params.id
 
     if (id === "coding") {
-       return res.status(200).json({ coding:coding })
+       return res.status(200).json({ 'coding':coding })
     }
 
     if (id === "genral") {
-       return res.status(200).json({ genral:genral })
+       return res.status(200).json({ 'genral':genral })
     }
 
     if (id === "math") {
-       return res.status(200).json({ math:math })
+       return res.status(200).json({ 'math':math })
     }
     return res.status(404).json({success:false,message:"quize not found"})
 
